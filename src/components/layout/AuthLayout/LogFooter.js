6807f89513b0e59dafb20de447f40e98dda10061
JereMitch <jere.mitch8@gmail.com>
2022-02-08 15:34:59 -0700

    cleaned up auth footer

--- src/components/layout/AuthLayout/LogFooter.js
+++ src/components/layout/AuthLayout/LogFooter.js
@@ -8,25 +8,6 @@ import { MdPsychology } from "react-icons/md";
 function LogFooter() {
   return (
     <div className="footer-container">
-      {/* <section className='footer-subscription'>
-        <p className='footer-subscription-heading font'>
-        Join our exclusive membership to receive the latest news and trends
-        </p>
-        <p className='footer-subscription-text font'>
-        You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-        <form>
-        <input
-        className='footer-input font'
-        name='email'
-        type='email'
-        placeholder='Your Email'
-        />
-        <Button buttonStyle='btn--outline' className='font'>Subscribe</Button>
-        </form>
-        </div>
-      </section> */}
       <div className="footer-links">
         <Link to="/lh" className="foot-logo">
           <img
@@ -55,48 +36,23 @@ function LogFooter() {
             </Link>
           </div>
           <div className="footer-contact">
-            <h3 className='font'>Contact Us:</h3>
+            <h3 className="font">Contact Us:</h3>
             <p className="phone font">
               Phone: <a href="/">(385) 485-6731</a>
             </p>
             <p className="address font">
-              Silver River Counseling <br/>63 North 300 East Suite 1E<br/> Provo, UT 84606
+              Silver River Counseling <br />
+              63 North 300 East Suite 1E
+              <br /> Provo, UT 84606
             </p>
           </div>
-          {/* <div className='footer-link-items'>
-            <h2 className='font'>Contact Us</h2>
-            <Link to='/' className='font'>Contact</Link>
-            <Link to='/' className='font'>Support</Link>
-            <Link to='/' className='font'>Destinations</Link>
-            <Link to='/' className='font'>Sponsorships</Link>
-          </div> */}
         </div>
-        {/* <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2 className='font'>Videos</h2>
-            <Link to='/' className='font'>Submit Video</Link>
-            <Link to='/' className='font'>Ambassadors</Link>
-            <Link to='/' className='font'>Agency</Link>
-            <Link to='/' className='font'>Influencer</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2 className='font'>Social Media</h2>
-            <Link to='/' className='font'>Instagram</Link>
-            <Link to='/' className='font'>Facebook</Link>
-            <Link to='/' className='font'>Youtube</Link>
-            <Link to='/' className='font'>Twitter</Link>
-          </div>
-        </div> */}
       </div>
       <section className="social-media">
         <div className="social-media-wrap">
-          {/* <div className='footer-logo'>
-            <Link to='/' className='social-logo font'>
-              <MdFingerprint className='navbar-icon' />
-              LAVISH
-            </Link>
-          </div> */}
-          <small className="website-rights font">Silver River Counseling © 2020</small>
+          <small className="website-rights font">
+            Silver River Counseling © 2020
+          </small>
           <div className="social-icons">
             <Link
               className="social-icon-link"
@@ -106,24 +62,7 @@ function LogFooter() {
             >
               <FaFacebook />
             </Link>
-            {/* <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <FaInstagram />
-            </Link> */}
-            {/* <Link
-              className='social-icon-link'
-              to={
-                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
-              }
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <FaYoutube />
-            </Link> */}
+
             <Link
               className="social-icon-link"
               to={
@@ -134,14 +73,6 @@ function LogFooter() {
             >
               <MdPsychology />
             </Link>
-            {/* <Link
-              className='social-icon-link'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <FaLinkedin />
-            </Link> */}
           </div>
         </div>
       </section>
