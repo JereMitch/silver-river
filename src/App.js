2bd27fdcce04d155f852f78a5f9e9e807ffbc52e
JereMitch <jere.mitch8@gmail.com>
2022-02-09 11:25:48 -0700

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import { Container } from "react-bootstrap";
-// import { AuthProvider } from "./contexts/AuthContext";
+
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import About from "./components/pages/About";
 import PaymentInsurance from "./components/pages/PaymentInsurance";
-// import MainNav from "./components/layout/MainNav";
-// import Footer from "./components/layout/Footer";
-import Thanks from './components/pages/Thanks'
+import Thanks from "./components/pages/Thanks";
 import SignUp from "./components/login/SignUp";
 import Login from "./components/login/Login";
 import PrivateRoute from "./components/login/PrivateRoute";
-// import UpdateProfile from "./components/login/UpdateProfile";
-import ForgotPassword from './components/login/ForgotPassword'
-import LogHome from './components/pages/AuthPages/LogHome'
-import LogServices from './components/pages/AuthPages/LogServices'
-import LogAbout from './components/pages/AuthPages/LogAbout'
-import LogPaymentInsurance from './components/pages/AuthPages/LogPaymentInsurance'
-import LogThanks from './components/pages/AuthPages/LogThanks'
+import ForgotPassword from "./components/login/ForgotPassword";
+import LogHome from "./components/pages/AuthPages/LogHome";
+import LogServices from "./components/pages/AuthPages/LogServices";
+import LogAbout from "./components/pages/AuthPages/LogAbout";
+import LogPaymentInsurance from "./components/pages/AuthPages/LogPaymentInsurance";
+import LogThanks from "./components/pages/AuthPages/LogThanks";
 import ScrollToTop from "./components/ScrollToTop";
-
-
 
 function App() {
   return (
@@ -34,19 +28,16 @@ function App() {
           <Route path="/payment" component={PaymentInsurance} />
           <Route path="/thanks" component={Thanks} />
 
-
           <Route path="/signup" component={SignUp} />
           <Route path="/login" component={Login} />
           <Route path="/forgot-password" component={ForgotPassword} />
 
-
-          <PrivateRoute path='/lh' component={LogHome} />
-          <PrivateRoute path='/ls' component={LogServices} />
-          <PrivateRoute path='/la' component={LogAbout} />
-          <PrivateRoute path='/lp' component={LogPaymentInsurance} />
-          <PrivateRoute path='/lt' component={LogThanks} />
+          <PrivateRoute path="/lh" component={LogHome} />
+          <PrivateRoute path="/ls" component={LogServices} />
+          <PrivateRoute path="/la" component={LogAbout} />
+          <PrivateRoute path="/lp" component={LogPaymentInsurance} />
+          <PrivateRoute path="/lt" component={LogThanks} />
         </Switch>
-        {/* </AuthProvider> */}
       </Router>
     </>
   );
